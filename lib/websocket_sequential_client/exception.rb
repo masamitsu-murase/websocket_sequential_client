226622538b0fa9db9d39e3270bef81eb22c204f0
Masamitsu MURASE <masamitsu.murase@gmail.com>
2015-05-06 17:13:03 +0900

    Add documentation or error classes.

--- lib/websocket_sequential_client/exception.rb
+++ lib/websocket_sequential_client/exception.rb
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 
 module WebsocketSequentialClient
+  #
+  # Base errror class
+  #
   class Error < StandardError
   end
 
+  #
+  # This error is raised when <tt>recv</tt> or <tt>send</tt> is called after the socket is closed.
+  #
   class SocketAlreadyClosed < Error
   end
 
+  #
+  # This error is raised when handshake with a server failed.
+  #
   class HandshakeFailed < Error
   end
 
+  #
+  # This error is raised when a invaid data is received.
+  #
   class InvalidDataReceived < Error
   end
 end
